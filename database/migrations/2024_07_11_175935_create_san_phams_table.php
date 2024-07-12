9bdd41778d35b16af066636a0361b379bf809726
hoangtuyen2004 <ductuyen772@gmail.com>
2024-07-12 09:46:43 +0700

    Creare table

---
+++ database/migrations/2024_07_11_175935_create_san_phams_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('san_phams', function (Blueprint $table) {
+            $table->id();
+            $table->string('ma_san_pham',10)->unique();
+            $table->string('ten_san_pham',255);
+            $table->string('anh_san_pham',255);
+            $table->string('mo_ta_san_pham',555);
+            $table->double('gia_san_pham');
+            $table->string('ma_danh_mucs',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('san_phams');
+    }
+};
