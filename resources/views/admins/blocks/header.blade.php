bc8c3349e87b0efd7f565ea67b92cb2d4762d489
duyhocit <hoangduyoffline@gmail.com>
2024-07-28 23:58:45 +0700

    vv

--- resources/views/admins/blocks/header.blade.php
+++ resources/views/admins/blocks/header.blade.php
@@ -63,7 +63,7 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="auth-sign-in-social.html">
+                                <a href="{{route('home')}}">
                                     <i class="feather icon-log-out"></i> Logout
                                 </a>
                             </li>
